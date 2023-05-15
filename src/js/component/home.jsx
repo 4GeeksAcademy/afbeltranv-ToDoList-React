979eb9e582896d78d47fff7a80d02fde18c16782
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-05-15 22:56:45 +0000

    Primer commit, se genera form y se logra useState con valor de input. Falta poder hacer push del elemento al array

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
-const Home = () => {
+const Home = () => {	
+	const [ToDoText, setToDoText]=useState("");
+	const [ToDoArray, setToDoArray]=useState(["a","b"]);
+
+	const AddTask=(ToDoText)=>{
+		setToDoArray(...current, "task:{ToDoText}")
+	}
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<form>
+				<div className="container">
+					<div className="row">
+						<h1>To-Do's</h1>
+						<div className="form-group col">						
+							<input type="text" onChange={(e)=>setToDoText(e.target.value)} value={ToDoText} className="form-control" id="ToDoInput" aria-describedby="emailHelp" placeholder="Enter To-Do" />							
+							<button onClick={AddTask}>Add task</button>
+						</div>												
+					</div>
+					<div id="ToDo-list">
+						<p>{ToDoText}</p>
+						<p>{ToDoArray}</p>
+					</div>					
+				</div>
+			</form>
 		</div>
-	);
+	);	
 };
 
 export default Home;
