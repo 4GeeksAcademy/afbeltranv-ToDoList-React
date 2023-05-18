be815481820e9f881dc4409fcd173ebff6e55f31
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-05-18 01:54:46 +0000

    Proyecto con todas las funcionalidades

---
+++ src/js/component/ToDo.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+const ToDo=()=>{
+    const[ToDoTask, setToDoTask]=useState("")
+    const[ToDoTaskList, setToDoTaskList]=useState([])
+
+return(
+    <>
+    <div className="container">
+    <h1>ToDo List {ToDoTask}</h1>
+    <ul>
+        <li><input 
+                type="text" 
+                onChange={(e)=>setToDoTask(e.target.value)} 
+                value={ToDoTask} 
+                placeholder="Type your task"
+                onKeyDown={(e)=> {if(e.key==="Enter" && ToDoTask!=""){
+                    setToDoTaskList(ToDoTaskList.concat(ToDoTask));
+                    setToDoTask("");                    
+                }}} >
+            </input>                
+        
+        </li>
+        {ToDoTaskList.map((task, index)=>
+                    (<li key={index}>
+                        {task}{" "} <span onClick={() => 
+                            setToDoTaskList(
+                                ToDoTaskList.filter(
+                                    (t, currentIndex)=>
+                                    index!=currentIndex))} > <i className="fa-solid fa-trash" ></i></span>
+                    </li>))            
+        } 
+        
+        </ul>
+        <div>{ToDoTaskList.length} tasks in list</div>
+    </div>
+    </>
+)
+
+}
+
+export default ToDo;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-
-
-//create your first component
-const Home = () => {	
-	const [ToDoText, setToDoText]=useState("");
-	const [ToDoArray, setToDoArray]=useState(["a","b","c"]);
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 
-	function AddTask(e){		
-		setToDoArray([...ToDoArray, ToDoText]);
-		e.preventDefault();
-		setToDoText("");
-	}
-	function DeleteTask(task){	
-		
-		console.log(task)	
 
-	}
-	return (
-		<div className="text-center">
-			<form>
-				<div className="container">
-					<div className="row">
-						<h1>To-Do's</h1>
-						<div className="form-group col">						
-							<input type="text" onChange={(e)=>setToDoText(e.target.value)} value={ToDoText} className="form-control" id="ToDoInput" placeholder="Enter To-Do" />							
+// //create your first component
+// const Home = () => {	
+// 	const [ToDoText, setToDoText]=useState("");
+// 	const [ToDoArray, setToDoArray]=useState(["a","b","c"]);
+
+
+// 	function AddTask(e){		
+// 		setToDoArray([...ToDoArray, ToDoText]);
+// 		e.preventDefault();
+// 		setToDoText("");
+// 	}
+// 	function DeleteTask(task){
+// 		ToDoArray.filter()		
+// 		console.log(task)	
+
+// 	}
+// 	return (
+// 		<div className="text-center">
+// 			<form>
+// 				<div className="container">
+// 					<div className="row">
+// 						<h1>To-Do's</h1>
+// 						<div className="form-group col">						
+// 							<input type="text" onChange={(e)=>setToDoText(e.target.value)} value={ToDoText} className="form-control" id="ToDoInput" placeholder="Enter To-Do" />							
 							
-						</div>		
-						<button onClick={AddTask}>Add task</button>										
-					</div>
-					<div id="ToDo-list">
-						<p>{ToDoText}</p>
-						<p>{ToDoArray}</p>
-						<div className="container">
-							{ToDoArray.map((task)=><div> {task} <span onClick={() => DeleteTask(task)}><i className="fa-solid fa-xmark"></i></span></div> )}
-						</div>
-					</div>					
-				</div>
-			</form>
-		</div>
-	);	
-};
+// 						</div>		
+// 						<button onClick={AddTask}>Add task</button>										
+// 					</div>
+// 					<div id="ToDo-list">
+// 						<p>{ToDoText}</p>
+// 						<p>{ToDoArray}</p>
+// 						<div className="container">
+// 							{ToDoArray.map((task)=><div> {task} <span id={task} key={task} onClick={() => DeleteTask(task)}><i className="fa-solid fa-xmark"></i></span></div> )}
+// 						</div>
+// 					</div>					
+// 				</div>
+// 			</form>
+// 		</div>
+// 	);	
+// };
 
-export default Home;
+// export default Home;
